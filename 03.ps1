4a76a966ff2bf426c94682a893235e7046a91b71
adrigarro <a21adrrodnav@inspedralbes.cat>
2022-02-10 15:51:06 +0100

    el 3 again

--- 03.ps1
+++ 03.ps1
@@ -1,2 +1,2 @@
 #Crea la carpeta myTest des de PowerShell, al mateix lloc on et trobes
-PS C:\Users\argo> New-Item -Path . -Name 'myTest' -ItemType Directory
+New-Item -Path . -Name 'myTest' -ItemType Directory
