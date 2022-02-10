793dde33e7b1eff8135cbb4974d33be4ba58ae64
adrigarro <a21adrrodnav@inspedralbes.cat>
2022-02-10 16:21:32 +0100

    9

--- 09.ps1
+++ 09.ps1
@@ -1,1 +1,8 @@
 #si dos numeros passats per parametre a l'script son iguals mostra OK. Si son diferents mostra KO
+if (Sparam[0] == Sparam[1])
+{
+    Write-Host "OK"
+}
+else {
+    Write-Host "KO"
+}
